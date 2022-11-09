099129d03ea153d4b14df1e007839dab625d2a9c
sondrp <sondrp@stud.ntnu.no>
2022-11-09 11:32:51 +0000

    make sure squares gets on click defined in board

--- chess/src/components/Board.js
+++ chess/src/components/Board.js
@@ -1,9 +1,25 @@
+import React from 'react'
+import Square from "./Square"
+/* 
+It seems like it is best to define state of the game here, and also give the function to each 
+
+
+*/
+
+function handleClick(boardArray, index) {
+    console.log(boardArray[index])
+}
+
 
 
 export default function Board(props) {
+    const boardArray = props.array;
+    const [squares, setSquares] = React.useState(boardArray.map((text, key) => <Square key={key} index={key} text={text} handleClick={() => handleClick(boardArray, key)}/>))
+
+    console.log("board was rendered")
     return (
         <div className="board">
-            {props.squares}
+            {squares}
         </div>
     )
 }

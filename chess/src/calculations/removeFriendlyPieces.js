2e9af600060266703337109c48aa56bac0fd7d08
sondrp <sondrp@stud.ntnu.no>
2022-11-12 09:43:43 +0000

    fix  small bug

--- chess/src/calculations/removeFriendlyPieces.js
+++ chess/src/calculations/removeFriendlyPieces.js
@@ -1,5 +1,6 @@
 
 export default function removeFriendlyPieces(boardarray, index, cover) {
+    console.log(cover)
     const piece = boardarray[index]
     const isWhite = piece === piece.toUpperCase()
     let moves = []
@@ -7,9 +8,10 @@ export default function removeFriendlyPieces(boardarray, index, cover) {
     // include all moves that are either empty, or have a piece with the same case as our main piece
     for (const i of cover) {
         let enemy = boardarray[i]
-        if (!enemy || (enemy !== enemy.toUpperCase()) === isWhite) {
+        if (!enemy || (enemy === enemy.toUpperCase()) !== isWhite) {
             moves.push(i)
         }
     }
+    console.log(moves)
     return moves
 }
